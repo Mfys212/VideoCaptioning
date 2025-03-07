a87fdc889d7990d0deb448b3d707e0b5fbb9330b
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-07 13:58:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,19 +32,38 @@ class CreateModel():
         self.train_data = train_dataset
         self.test_data = valid_dataset
 
-    def DefineModel(self, D_MODELS, NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, 
+    def DefineModel(self, ENCODER, DECODER, D_MODELS, NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, 
                     NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L):
-        encoder = TransformerEncoderBlock(d_models=D_MODELS, num_heads=NUM_HEADS, 
+        encoder = CrossAttention.Encoder(d_models=D_MODELS, num_heads=NUM_HEADS, 
                                           num_l=NUM_L, max_frames=MAX_FRAMES)
         encoder.build(input_shape=(None, MAX_FRAMES, *SPATIAL_SIZE, 3))  
-        decoder = TransformerDecoderBlock(d_models=D_MODELS, num_heads=NUM_HEADS, 
-                                          vocab_size=VOCAB_SIZE, seq_length=SEQ_LENGTH, num_l=NUM_L)
+        decoder = module.Decoder(d_models=D_MODELS, num_heads=NUM_HEADS, 
+                                vocab_size=VOCAB_SIZE, seq_length=SEQ_LENGTH, num_l=NUM_L)
         decoder.build([(None, None), (None, NUM_PATCH, D_MODELS), (None, None)])
         model = MainModel(encoder=encoder, decoder=decoder)
         return encoder, decoder, model
 
-    def SpatioTemporalAttention(self):
-        pass
+    def SpatioTemporalAttention(self, D_MODELS, NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, 
+                               NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L):
+        if self.multigpu == True:
+            with strategy.scope():
+                encoder, decoder, model = self.DefineModel(SpatioTemporalAttention.Encoder, module.Decoder, D_MODELS, 
+                                                           NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)
+        else:
+            encoder, decoder, model = self.DefineModel(SpatioTemporalAttention.Encoder, module.Decoder, D_MODELS, 
+                                                       NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)  
+        self.D_MODELS = D_MODELS
+        self.SEQ_LENGTH = SEQ_LENGTH
+        self.VOCAB_SIZE = VOCAB_SIZE
+        self.SPATIAL_SIZE = SPATIAL_SIZE
+        self.MAX_FRAMES = MAX_FRAMES
+        trainable_vars = model.trainable_variables
+        total_params = 0
+        for var in trainable_vars:
+            var_params = tf.size(var).numpy()
+            total_params += var_params
+        print(f"Num of trainable parameters: {total_params}")
+        self.model = model
 
     def FactorisedEncoder(self):
         pass
@@ -59,11 +78,11 @@ class CreateModel():
                        NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L):
         if self.multigpu == True:
             with strategy.scope():
-                encoder, decoder, model = self.DefineModel(D_MODELS, NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, 
-                                                            NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)
+                encoder, decoder, model = self.DefineModel(CrossAttention.Encoder, module.Decoder, D_MODELS, 
+                                                           NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)
         else:
-            encoder, decoder, model = self.DefineModel(D_MODELS, NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, 
-                                                        NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)  
+            encoder, decoder, model = self.DefineModel(CrossAttention.Encoder, module.Decoder, D_MODELS, 
+                                                       NUM_HEADS, MAX_FRAMES, SPATIAL_SIZE, NUM_PATCH, VOCAB_SIZE, SEQ_LENGTH, NUM_L)  
         self.D_MODELS = D_MODELS
         self.SEQ_LENGTH = SEQ_LENGTH
         self.VOCAB_SIZE = VOCAB_SIZE
