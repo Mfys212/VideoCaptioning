a21669e1bf01944b8b9b0fa4e4d01e80a12e5b32
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-08 17:48:12 +0700

    Update FactorisedEncoder.py

--- model/FactorisedEncoder.py
+++ model/FactorisedEncoder.py
@@ -17,7 +17,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         attention = self.attention(query=Z, value=Z, key=Z, attention_mask=mask, training=training)
         Z = self.layernorm[0](layers.Add()([Z, attention]))
         ffn = self.densel(self.dropout(self.dense(Z), training=training))
-        Z = self.layernorm[2](layers.Add()([Z, ffn]))
+        Z = self.layernorm[1](layers.Add()([Z, ffn]))
         return Z
 
 class Encoder(tf.keras.Model):
@@ -29,12 +29,13 @@ class Encoder(tf.keras.Model):
         self.Tpositional_encoding = PositionalEncoding(sequence_length=max_frames, embed_dim=d_models)
         self.blocks_spatial = [EncoderBlock(d_models, num_heads) for _ in range(num_l)]
         self.blocks_temporal = [EncoderBlock(d_models, num_heads) for _ in range(num_l)]
+        self.max_frames = max_frames
 
     def call(self, inputs, training=True, mask=None):
-        Z = tf.split(inputs, num_or_size_splits=4, axis=1)
+        Z = tf.split(inputs, num_or_size_splits=self.max_frames, axis=1)
         Z = [self.patch_embedding(z) for z in Z]
-        for i in range(len(Z)):    
-            z = layers.Add()([Z[i], self.Spositional_encoding(Z[i])])
+        Z = [layers.Add()([z, self.Spositional_encoding(z)]) for z in Z]
+        for i in range(len(Z)):
             for block in self.blocks_spatial:
                 z = block(z, mask, training)
             Z[i] = z
