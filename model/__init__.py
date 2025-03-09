20162f099c96b41200d2991a9a9db1375ac4723e
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-09 23:07:58 +0700

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -4,8 +4,8 @@ from .SpatioTemporalAttention import *
 from .FactorisedEncoder import *
 from .FactorisedSelfAttention import *
 from .FactorisedDotProductAttention import *
-import .CrossAttention 
-import .SpatioTemporalAttention 
-import .FactorisedEncoder
-import .FactorisedSelfAttention 
-import .FactorisedDotProductAttention 
+import CrossAttention 
+import SpatioTemporalAttention 
+import FactorisedEncoder
+import FactorisedSelfAttention 
+import FactorisedDotProductAttention 
