002303ab62a70fe196a6ca6efb14956d46f27c87
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-10 19:19:46 +0700

    Update FactorisedSelfAttention.py

--- model/FactorisedSelfAttention.py
+++ model/FactorisedSelfAttention.py
@@ -9,43 +9,46 @@ class EncoderBlock(tf.keras.layers.Layer):
         d = d_models // num_heads
         self.attention1 = layers.MultiHeadAttention(num_heads=num_heads, key_dim=d, dropout=0.1)
         self.attention2 = layers.MultiHeadAttention(num_heads=num_heads, key_dim=d, dropout=0.1)
-        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm3 = layers.LayerNormalization(epsilon=1e-6)
-        self.dense1 = layers.Dense(d_models, activation="gelu")
-        self.dense2 = layers.Dense(d_models)
+        self.layernorm = [layers.LayerNormalization(epsilon=1e-6) for _ in range(3)]
+        self.dense = layers.Dense(d_models, activation="gelu")
+        self.densel = layers.Dense(d_models)
         self.dropout = layers.Dropout(0.1)
 
     def call(self, Z, mask=None, training=True):
-        Z = tf.stack(Z, axis=1)
-        z_norm = self.layernorm1(Z)
-        attn1 = self.attention1(query=z_norm, value=z_norm, key=z_norm, attention_mask=mask, training=training)
-        Z = layers.Add()([Z, attn1])
-        Z = tf.transpose(Z, perm=[0, 2, 1, 3])
-        z_norm = self.layernorm2(Z)
-        attn2 = self.attention2(query=z_norm, value=z_norm, key=z_norm, attention_mask=mask, training=training)
-        Z = layers.Add()([Z, attn2])
-        z_norm = self.layernorm3(Z)
-        ffn_output = self.dense2(self.dropout(self.dense1(z_norm), training=training))
-        Z = layers.Add()([Z, ffn_output])
-        Z = tf.transpose(Z, perm=[0, 2, 1, 3])
-        return tf.reshape(Z, [tf.shape(Z)[0], -1, tf.shape(Z)[-1]])
+        Z_new = []
+        for z in Z:
+            z_norm = self.layernorm[0](z)
+            attention = self.attention1(query=z_norm, value=z_norm, key=z_norm, attention_mask=mask, training=training)
+            Z_new.append(layers.Add()([z, attention]))
+        Z = tf.stack(Z_new, axis=1)
+        num_patch = int(tf.shape(Z)[2])
+        Z_split = tf.split(Z, num_or_size_splits=num_patch, axis=2)
+        Z_new = []
+        for z in Z_split:
+            z_norm = self.layernorm[1](z)
+            attention = self.attention2(query=z_norm, value=z_norm, key=z_norm, attention_mask=mask, training=training)
+            z = layers.Add()([z, attention])
+            ffn = self.densel(self.dropout(self.dense(self.layernorm[2](z)), training=training))
+            Z_new.append(layers.Add()([z, ffn]))
+        Z = tf.concat(Z_new, axis=1)
+        return Z
 
-class Encoder(tf.keras.layers.Layer):
+class Encoder(tf.keras.Model):
     def __init__(self, d_models, num_heads, num_l, max_frames, spatial_size, **kwargs):
         super().__init__(**kwargs)
         self.patch_embedding = PatchEmbedding(d_models, 1, 16, 16)
-        self.spa_num_patch = (spatial_size // 16) ** 2
+        self.spa_num_patch = int((spatial_size / 16) ** 2)
         self.Spositional_encoding = PositionalEncoding(sequence_length=self.spa_num_patch, embed_dim=d_models)
-        self.Tpositional_encoding = PositionalEncoding(sequence_length=max_frames, embed_dim=d_models)
         self.blocks = [EncoderBlock(d_models, num_heads) for _ in range(num_l)]
         self.max_frames = max_frames
         self.d_models = d_models
 
     def call(self, inputs, training=True, mask=None):
-        Z = tf.reshape(inputs, [-1, self.max_frames, self.spa_num_patch, self.d_models])
-        Z = self.patch_embedding(Z)
-        Z += self.Spositional_encoding(Z)
+        Z = tf.split(inputs, num_or_size_splits=self.max_frames, axis=1)
+        Z = [self.patch_embedding(z) for z in Z]
+        Z = [layers.Add()([z, self.Spositional_encoding(z)]) for z in Z]
         for block in self.blocks:
-            Z = block(Z, mask=mask, training=training)
-        return tf.reshape(Z, [tf.shape(inputs)[0], -1, self.d_models])
+            Z = block(Z, training, mask)
+            Z = tf.split(Z, num_or_size_splits=self.max_frames, axis=1)
+        Z = tf.reshape(tf.concat(Z, axis=2), (tf.shape(inputs)[0], self.max_frames * self.spa_num_patch, self.d_models))
+        return Z
