8383d1a763845fea2c5ba92d319ccda5889215b0
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-11 14:07:43 +0700

    Update SpatioTemporalAttention.py

--- model/SpatioTemporalAttention.py
+++ model/SpatioTemporalAttention.py
@@ -23,8 +23,8 @@ class EncoderBlock(tf.keras.layers.Layer):
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, d_models, num_heads, num_l, max_frames, spatial_size, **kwargs):
         super().__init__(**kwargs)
-        self.patch_embedding = PatchEmbedding(d_models, 1, 16, 16)
-        num_patch = int((max_frames*spatial_size**2) / (1*16*16))
+        self.patch_embedding = PatchEmbedding(d_models, 2, 16, 16)
+        num_patch = int((max_frames*spatial_size**2) / (2*16*16))
         self.positional_encoding = PositionalEncoding(sequence_length=num_patch, embed_dim=d_models)
         self.blocks = [EncoderBlock(d_models, num_heads) for _ in range(num_l)]
 
