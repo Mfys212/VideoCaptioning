58f2b4134ee2e91804e4c3b002dfb8b535b16d91
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-03-29 20:02:55 +0700

    Update CrossAttention.py

--- model/CrossAttention.py
+++ model/CrossAttention.py
@@ -9,7 +9,7 @@ except:
     pass
 
 class EncoderBlock(tf.keras.layers.Layer):
-    def __init__(self, d_models, num_heads, **kwargs):
+    def __init__(self, d_models, num_heads, TEMPORAL_FIRST=True, **kwargs):
         super().__init__(**kwargs)
         d = d_models // num_heads
         self.attention1 = layers.MultiHeadAttention(num_heads=num_heads, key_dim=d, dropout=0.1)
@@ -18,15 +18,24 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.dense = layers.Dense(d_models, activation="gelu")
         self.densel = layers.Dense(d_models)
         self.dropout = layers.Dropout(0.1)
+        self.tem_first = TEMPORAL_FIRST
 
     def call(self, inputs, mask=None, training=True):
         Zt, Zs = inputs
-        Zts = tf.concat([Zs, Zt], axis=1)
-        attention1 = self.attention1(query=Zt, value=Zts, key=Zts, attention_mask=mask, training=training)
-        Zt = self.layernorm[0](layers.Add()([Zt, attention1]))
-        Zst = tf.concat([Zs, Zt], axis=1)
-        attention2 = self.attention2(query=Zs, value=Zst, key=Zst, attention_mask=mask, training=training)
-        Zs = self.layernorm[1](layers.Add()([Zs, attention2]))
+        if self.tem_first:
+            Zts = tf.concat([Zs, Zt], axis=1)
+            attention1 = self.attention1(query=Zt, value=Zts, key=Zts, attention_mask=mask, training=training)
+            Zt = self.layernorm[0](layers.Add()([Zt, attention1]))
+            Zst = tf.concat([Zs, Zt], axis=1)
+            attention2 = self.attention2(query=Zs, value=Zst, key=Zst, attention_mask=mask, training=training)
+            Zs = self.layernorm[1](layers.Add()([Zs, attention2]))
+        else:
+            Zts = tf.concat([Zs, Zt], axis=1)
+            attention1 = self.attention1(query=Zs, value=Zts, key=Zts, attention_mask=mask, training=training)
+            Zs = self.layernorm[0](layers.Add()([Zs, attention1]))
+            Zst = tf.concat([Zs, Zt], axis=1)
+            attention2 = self.attention2(query=Zt, value=Zst, key=Zst, attention_mask=mask, training=training)
+            Zt = self.layernorm[1](layers.Add()([Zs, attention2]))
 
         ffn1 = self.densel(self.dropout(self.dense(Zt), training=training))
         Zt = self.layernorm[2](layers.Add()([ffn1, Zt]))
@@ -35,7 +44,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         return [Zt, Zs]
 
 class Encoder(tf.keras.models.Model):
-    def __init__(self, d_models, num_heads, num_l, max_frames, spatial_size, R_SPATIAL=4, R_TEMPORAL=4, F_SPATIAL=4, F_TEMPORAL=4, **kwargs):
+    def __init__(self, d_models, num_heads, num_l, max_frames, spatial_size, R_SPATIAL=4, R_TEMPORAL=4, F_SPATIAL=4, F_TEMPORAL=4, TEMPORAL_FIRST=True, **kwargs):
         super().__init__(**kwargs)
         self.tconv1 = layers.Conv3D(F_SPATIAL, (1, int(R_SPATIAL//2)+1, int(R_SPATIAL//2)+1), strides=(1, int(R_SPATIAL//2), int(R_SPATIAL//2)), padding='same')
         self.tconv2 = layers.Conv3D(3, (2, int(R_SPATIAL//2)+1, int(R_SPATIAL//2)+1), strides=(2, int(R_SPATIAL//2), int(R_SPATIAL//2)), padding='same')
@@ -47,7 +56,7 @@ class Encoder(tf.keras.models.Model):
         num_patch = int((max_frames*spatial_size**2) / (max_frames*16*16))
         self.t_positional_encoding = PositionalEncoding(sequence_length=max_frames//2, embed_dim=d_models)
         self.s_positional_encoding = PositionalEncoding(sequence_length=num_patch, embed_dim=d_models)
-        self.blocks = [EncoderBlock(d_models, num_heads) for _ in range(num_l)]
+        self.blocks = [EncoderBlock(d_models, num_heads, TEMPORAL_FIRST) for _ in range(num_l)]
         self.out = layers.Dense(d_models, activation="gelu")
 
     def call(self, inputs, training=True, mask=None):
