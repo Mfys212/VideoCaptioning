b096f4976a106fc66bafaccdf78b204b3e7f3528
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2025-07-20 14:34:18 +0700

    Update FactorisedEncoder.py

--- model/FactorisedEncoder.py
+++ model/FactorisedEncoder.py
@@ -13,16 +13,17 @@ class EncoderBlock(tf.keras.layers.Layer):
         super().__init__(**kwargs)
         d = d_models // num_heads
         self.attention = layers.MultiHeadAttention(num_heads=num_heads, key_dim=d, dropout=0.1)
-        self.layernorm = [layers.LayerNormalization() for _ in range(2)]
+        self.layernorm = [layers.LayerNormalization() for _ in range(3)]
         self.dense = layers.Dense(d_models, activation="gelu")
         self.densel = layers.Dense(d_models)
         self.dropout = layers.Dropout(0.1)
 
     def call(self, Z, mask=None, training=True):
-        attention = self.attention(query=Z, value=Z, key=Z, attention_mask=mask, training=training)
-        Z = self.layernorm[0](layers.Add()([Z, attention]))
+        Z_norm = self.layernorm[0](Z)
+        attention = self.attention(query=Z_norm, value=Z_norm, key=Z_norm, attention_mask=mask, training=training)
+        Z = self.layernorm[1](layers.Add()([Z, attention]))
         ffn = self.densel(self.dropout(self.dense(Z), training=training))
-        Z = self.layernorm[1](layers.Add()([Z, ffn]))
+        Z = self.layernorm[2](layers.Add()([Z, ffn]))
         return Z
 
 class Encoder(tf.keras.models.Model):
